4d01ad41f42ac2b2aeaca97a5878fbdd8f527010
pedrofnseca <pedropereira.pp264@gmail.com>
2022-07-11 15:26:12 -0300

    Imprimindo tentativas

--- index.js
+++ index.js
@@ -5,18 +5,65 @@ var palavras = [
    'pedro'
 ]
 
+var letrasTentativas = []
+var palaraEscolhida
+
+const inputLetra = document.getElementById('letraUser')
 const divLetrasErradas = document.getElementById('letrasErradas')
 const divPalavra = document.getElementById('palavra')
 
 function carregar(){
     const aux = Math.floor(Math.random() * palavras.length)
 
-    var palaraEscolhida = palavras[aux]
+    palaraEscolhida = palavras[aux]
 
     divPalavra.innerHTML = ''
     for(let i = 0; i <= palaraEscolhida.length; i++){
         divPalavra.innerHTML += ' _'
     }
+}
 
-    divLetrasErradas.innerHTML = `<h1>${palaraEscolhida}<h1>`
+function actionbtn(){
+    let letra = inputLetra.value.toUpperCase()
+    
+    // Tratamento para tentativas invalidas
+    if(letra.length != 1){
+        return alert('Digite uma letra')
+    }
+    else if(letrasTentativas.includes(letra)){
+        return alert('Tentativa inválida')
+    }
+
+    letrasTentativas.push(letra)
+    imprimePalavra()
+
+    // Imprime as tentativas
+    let LetrasErradasFormatadas = ''
+    for(let i = 0; i < letrasTentativas.length; i++){
+        if(i == 0){
+            LetrasErradasFormatadas = ` ${letrasTentativas[i]}`
+        }
+        else{
+            LetrasErradasFormatadas += `-${letrasTentativas[i]}`
+        }
+    }
+
+    divLetrasErradas.innerHTML = `<h1> ${LetrasErradasFormatadas}</h1>`
+}
+
+function imprimePalavra(){
+    divPalavra.innerHTML = ''
+
+    for(let i = 0; i <= palaraEscolhida.length; i++){
+        let aux = true
+        for(let j = 0; j < letrasTentativas.length; j++){
+            if(palaraEscolhida[i] == letrasTentativas[j]){
+                aux = false
+                divPalavra.innerHTML += ` ${letrasTentativas[j]}`
+            }
+        }
+        if(aux){
+            divPalavra.innerHTML += ' _'
+        }
+    }
 }
